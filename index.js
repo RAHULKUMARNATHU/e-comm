757283c87a173f09c0cbc64ad291fd5f83dbfbd2
RAHUL KUMAR <rahulvaidya148@gmail.com>
2023-06-07 19:02:06 +0530

    commit :: initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const db = require('./databases/mongoose')
+const PORT = 8000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// app.use("/", require("./routes/index.js"));
+
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`);
+});
