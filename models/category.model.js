9f84bfc1cdbb078d2e71ad926fc0c733bca20b9c
RAHUL KUMAR <rahulvaidya148@gmail.com>
2023-06-07 19:48:49 +0530

    commit :: create seller

--- models/category.model.js
+++ models/category.model.js
@@ -1,12 +1,13 @@
 const mongoose = require("mongoose");
 
-//connection to DB
-mongoose.connect("mongodb://localhost/todo");
-const db = mongoose.connection;
-
-db.on("error", console.error.bind(console, "Error connecting to db"));
-
-db.once("open", function () {
-  console.log("Successfully connected to db");
-});
-module.exports = db;
+const categorySchema = mongoose.Schema({
+    name: {
+      type: String,
+	required: true,
+	unique: true
+    },
+    description: {
+		type: String,
+		required: true
+	},   
+})
--- routes/index.js
+++ routes/index.js
@@ -2,5 +2,6 @@ const express = require("express");
 const router = express.Router();
 
 router.use("/seller", require("./seller"));
+router.use("/store" ,require("./store"))
 
 module.exports = router;
