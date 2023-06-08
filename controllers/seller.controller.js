f27aab548e40e282cf3448973e60556b987f8f6c
vivek <vivek.raj@brainvire.com>
2023-06-08 10:51:33 +0530

    commit :: seller registration

--- controllers/seller.controller.js
+++ controllers/seller.controller.js
@@ -1,10 +1,38 @@
+const Seller = require("../models/seller.model");
+const jwt = require("jsonwebtoken");
+const secretKey = "Secret";
 
+//1. Seller SignUp
+const signUp = async (req, res) => {
+  try {
+    let seller = await Seller.create(req.body);
+    res.status(200).send(seller);
+  } catch (err) {
+    res.status(400).send(err.message);
+  }
+};
 
-const getSeller = async()=> {
+//2. Login
+const signIn = async (req, res) => {
+  try {
+    const user = req.body;
+    jwt.sign({ user }, secretKey, { expiresIn: "1d" }, (error, token) => {
+      if (error) {
+        res.status(400).json(error.message);
+      }
+      res.status(200).json({ token });
+    });
+  } catch (error) {
+    throw error;
+  }
+};
 
-}
-
-
-module.exports ={
-    getSeller
-}
+const findUser = async (req, res) => {
+  const isUser = await Seller.findOne({ email: req.body.email });
+  return res.send(isUser);
+};
+module.exports = {
+  signUp,
+  signIn,
+  findUser,
+};
--- routes/seller.js
+++ routes/seller.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const sellerController = require("../controllers/seller.controller");
 
-router.get("/get", sellerController.getSeller);
+router.post("/signup" , sellerController.signUp);
+router.post("/signin", sellerController.signIn);
+router.post("/findUSer" , sellerController.findUser );
 
 module.exports = router;
